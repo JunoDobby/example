1cd37fb39abbc9f52766c1188633f3961aadd4b3
Dobby <junho.hur@jitcom.net>
2023-07-03 13:39:54 +0900

    database user설정

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->insert([
+            [
+                'id' => 1,
+                'name' => 'admin',
+                'email' => 'admin@jitcom.net',
+                'password' => bcrypt('adminadmin'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'admin2',
+                'email' => 'admin2@jitcom.net',
+                'password' => bcrypt('adminadmin2'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],[
+                'id' => 3,
+                'name' => 'admin3',
+                'email' => 'admin3@jitcom.net',
+                'password' => bcrypt('adminadmin3'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
